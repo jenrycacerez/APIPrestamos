b7c2df52ac617c15f73a53d97b90db9e63bc8147
jenry cacerez guzman <jenrybrown@hotmail.com>
2020-07-12 10:54:16 -0400

    Add project files.

---
+++ APiPrestamo/Controllers/PrestamosController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PrestamosProyect.BLL;
+using PrestamosProyect.Models;
+
+namespace APiPrestamo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrestamosController : ControllerBase
+    {
+        // GET: api/Prestamos
+        [HttpGet]
+        public ActionResult<List<Prestamos>> Get()
+        {
+            return PrestamosBLL.GetPrestamos();
+        }
+
+
+        // GET: api/Prestamos/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Prestamos
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Prestamos/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
